d83f1a0dfc796ceaf10e6c66a9c917c3d241a676
Gistyn <29015942+Jabster28@users.noreply.github.com>
2018-09-14 21:23:06 +0100

    Added some firebase auth functions

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,32 @@
 console.log("I'm index.js!")
+
+function siweap() {
+  console.log(54)
+  const email = $('#email').val()
+  console.log(email)
+  const pass = $('#password').val()
+  console.log(pass)
+  firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
+
+function SignOut() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
+
+function suweap() {
+  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
